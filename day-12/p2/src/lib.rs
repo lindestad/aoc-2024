254264770e05e4f0af532e0d8cc888a1f49500fe
Daniel Lindestad <lindestad@proton.me>
2025-01-10 23:57:36 +0100

    wip

--- day-12/p2/src/lib.rs
+++ day-12/p2/src/lib.rs
@@ -16,7 +16,7 @@ pub fn str_to_vec(input: &str) -> Vec<Vec<char>> {
     output
 }
 
-fn check_inbounts(x: i32, y: i32, width: i32, height: i32) -> bool {
+fn check_inbounds(x: i32, y: i32, width: i32, height: i32) -> bool {
     x >= 0 && x < width && y >= 0 && y < height
 }
 
@@ -39,7 +39,7 @@ pub fn find_single_area(
         for (dx, dy) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
             let new_x = x as i32 + dx;
             let new_y = y as i32 + dy;
-            if check_inbounts(new_x, new_y, grid[0].len() as i32, grid.len() as i32)
+            if check_inbounds(new_x, new_y, grid[0].len() as i32, grid.len() as i32)
                 && grid[new_y as usize][new_x as usize] == letter
             {
                 stack.push((new_x as usize, new_y as usize));
@@ -69,7 +69,7 @@ pub fn calculate_single_price(
         for (dx, dy) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
             let new_x = x as i32 + dx;
             let new_y = y as i32 + dy;
-            if check_inbounts(new_x, new_y, grid[0].len() as i32, grid.len() as i32)
+            if check_inbounds(new_x, new_y, grid[0].len() as i32, grid.len() as i32)
                 && grid[new_y as usize][new_x as usize] == letter
             {
                 stack.push((new_x as usize, new_y as usize));
@@ -93,4 +93,273 @@ pub fn calculate_price(input: &str) -> i32 {
         }
     }
     price
+}
+
+// pub fn find_edge(x: usize, y: usize, grid: &Vec<Vec<char>>) -> Vec<usize> {
+//     let letter = grid[y][x];
+//     let mut edges = vec![];
+//     for (i, (dx, dy)) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)].enumerate() {
+//         let new_x = x as i32 + dx;
+//         let new_y = y as i32 + dy;
+//         if check_inbounts(new_x, new_y, grid[0].len() as i32, grid.len() as i32)
+//             && grid[new_y as usize][new_x as usize] != letter
+//         {
+//             edges.push(grid[new_y as usize][new_x as usize] as usize);
+//         }
+//     }
+//     edges
+// }
+
+// pub fn find_edges(
+//     x: usize,
+//     y: usize,
+//     grid: &Vec<Vec<char>>,
+//     &mut visited: HashSet<(usize, usize)>,
+// ) -> HashMap<(usize, usize), Vec<usize>> {
+//     // Tread the grid (2D surface) as a surface with topoloigically
+//     // embedded structures. The edges are the points where the surface
+//     // is not flat, i.e. we embed them in the z axis. There can be multiple
+//     // edges at the same point, so we store a vector of them for each point
+//     // in the grid. We map the edges as a closed loop 1-4, where each neighboring
+//     // number is connected by an edge, we can then search for the neighboring edge
+//     // for any given edge as i _must_! exist. 1 connects to 2 or 4, etc.
+//     // The edges are stored as a tuple HashMap<(usize, usize), Vec<usize>>
+//     // where the key is the point in the grid and the value is the vector of edges.
+//     // a flat side is represented as a 0, no sides (internal) is not stored.
+//     let letter = grid[y][x];
+//     let mut edges: HashMap<(usize, usize), Vec<usize>> = HashMap::new();
+//     let mut stack = vec![(x, y)];
+//     while !stack.is_empty() {
+//         let (x, y) = stack.pop().unwrap();
+//         if visited.contains(&(x, y)) {
+//             continue;
+//         }
+//         visited.insert((x, y));
+//         area += 1;
+//         for (dx, dy) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)] {
+//             let new_x = x as i32 + dx;
+//             let new_y = y as i32 + dy;
+//             if check_inbounts(new_x, new_y, grid[0].len() as i32, grid.len() as i32)
+//                 && grid[new_y as usize][new_x as usize] == letter
+//             {
+//                 stack.push((new_x as usize, new_y as usize));
+//             } else {
+//                 fence += 1;
+//             }
+//         }
+//     }
+
+//     area * fence
+// }
+
+// pub fn count_corners(grid: &Vec<Vec<char>>) -> usize {
+//     let mut corners: usize = 0;
+//     let width = grid[0].len();
+//     let height = grid.len();
+//     // handle small grids
+//     if width == 0 {
+//         panic!("Empty grid provided");
+//     }
+
+//     // should not need this
+//     // if width + height == 3 || (width == 1 && height == 1) {
+//     //     if grid[0][0] == grid[width - 1][height - 1] {
+//     //         corners = 4;
+//     //     } else {
+//     //         corners = 8;
+//     //     }
+//     //     return corners;
+//     // }
+
+//     // Find perimeter corners to avoid checking validity of usize indexes
+//     for x in 0..width - 1 {
+//         // top row
+//         let letter = grid[0][x];
+//         if letter != grid[0][x + 1] {
+//             corners += 2;
+//         }
+//         if height > 1 {
+//             // inside corners
+//             if x < width - 1 {
+//                 // avoid overflow
+//                 if letter == grid[1][x] && letter == grid[0][x + 1] && letter != grid[1][x + 1] {
+//                     corners += 1;
+//                     dbg!(corners, "top row 1");
+//                 } else if letter != grid[1][x] && letter != grid[0][x + 1] {
+//                     corners += 1;
+//                     dbg!(corners, "top row 2");
+//                 }
+//             }
+//         }
+//     }
+//     for x in 0..width - 1 {
+//         // bottom row
+//         let letter = grid[height - 1][x];
+//         if letter != grid[height - 1][x + 1] {
+//             corners += 2;
+//         }
+//         if height > 1 {
+//             // inside corners
+//             if x < width - 1 {
+//                 // avoid overflow
+//                 if letter == grid[height - 2][x]
+//                     && letter == grid[height - 1][x + 1]
+//                     && letter != grid[height - 2][x + 1]
+//                 {
+//                     corners += 1;
+//                     dbg!(corners, "bottom row 1");
+//                 } else if letter != grid[height - 2][x] && letter != grid[height - 1][x + 1] {
+//                     corners += 1;
+//                     dbg!(corners, "bottom row 2");
+//                 }
+//             }
+//         }
+//     }
+//     for y in 0..height - 1 {
+//         // left column
+//         let letter = grid[y][0];
+//         if letter != grid[y + 1][0] {
+//             corners += 2;
+//         }
+//         if width > 1 {
+//             // inside corners
+//             if y < height - 1 {
+//                 // avoid overflow
+//                 if letter == grid[y][1] && letter == grid[y + 1][0] && letter != grid[y + 1][1] {
+//                     corners += 1;
+//                     dbg!(corners, "left column 1");
+//                 } else if letter != grid[y][1] && letter != grid[y + 1][0] {
+//                     corners += 1;
+//                     dbg!(corners, "left column 2");
+//                 }
+//             }
+//         }
+//     }
+//     for y in 0..height - 1 {
+//         // right column
+//         let letter = grid[y][width - 1];
+//         if letter != grid[y + 1][width - 1] {
+//             corners += 2;
+//         }
+//         if width > 1 {
+//             // inside corners
+//             if y < height - 1 {
+//                 // avoid overflow
+//                 if letter == grid[y][width - 2]
+//                     && letter == grid[y + 1][width - 1]
+//                     && letter != grid[y + 1][width - 2]
+//                 {
+//                     corners += 1;
+//                     dbg!(corners, "right column 1");
+//                 } else if letter != grid[y][width - 2] && letter != grid[y + 1][width - 1] {
+//                     corners += 1;
+//                     dbg!(corners, "right column 2");
+//                 }
+//             }
+//         }
+//     }
+//     corners += 4; // corners of the grid
+
+//     for y in 1..height - 1 {
+//         for x in 1..width - 1 {
+//             let letter = grid[y][x];
+//             let directions = [
+//                 [(-1, 0), (0, -1)], // Left & Up
+//                 [(1, 0), (0, -1)],  // Right & Up
+//                 [(-1, 0), (0, 1)],  // Left & Down
+//                 [(1, 0), (0, 1)],   // Right & Down
+//             ];
+//             for direction in &directions {
+//                 let dx1 = direction[0].0;
+//                 let dy1 = direction[0].1;
+//                 let dx2 = direction[1].0;
+//                 let dy2 = direction[1].1;
+//                 if letter == grid[(y as i32 + dy1) as usize][(x as i32 + dx1) as usize]
+//                     && letter == grid[(y as i32 + dy2) as usize][(x as i32 + dx2) as usize]
+//                     && letter
+//                         != grid[(y as i32 + dy1 + dy2) as usize][(x as i32 + dx1 + dx2) as usize]
+//                 {
+//                     // corner like this: _|X
+//                     // (exterior)        X X
+//                     corners += 1;
+//                 } else if letter != grid[(y as i32 + dy1) as usize][(x as i32 + dx1) as usize]
+//                     && letter != grid[(y as i32 + dy2) as usize][(x as i32 + dx2) as usize]
+//                 {
+//                     // corner like this   _
+//                     // (interior)        |X
+//                     corners += 1;
+//                 }
+//             }
+//         }
+//     }
+//     corners
+// }
+
+pub fn count_corners(grid: &Vec<Vec<char>>) -> usize {
+    let mut corners: usize = 0;
+    let width = grid[0].len();
+    let height = grid.len();
+    // handle small grids
+    if width == 0 {
+        panic!("Empty grid provided");
+    }
+
+    for y in 0..height {
+        for x in 0..width {
+            let letter = grid[y][x];
+            let directions = [
+                [(-1, 0), (0, -1)], // Left & Up
+                [(1, 0), (0, -1)],  // Right & Up
+                [(-1, 0), (0, 1)],  // Left & Down
+                [(1, 0), (0, 1)],   // Right & Down
+            ];
+            for direction in &directions {
+                let dx1 = direction[0].0;
+                let dy1 = direction[0].1;
+                let dx2 = direction[1].0;
+                let dy2 = direction[1].1;
+                if check_inbounds(
+                    (x as i32 + dx1),
+                    (y as i32 + dy1),
+                    width as i32,
+                    height as i32,
+                ) && check_inbounds(
+                    (x as i32 + dx2),
+                    (y as i32 + dy2),
+                    width as i32,
+                    height as i32,
+                ) && check_inbounds(
+                    (x as i32 + dx1 + dx2),
+                    (y as i32 + dy1 + dy2),
+                    width as i32,
+                    height as i32,
+                ) && letter == grid[(y as i32 + dy1) as usize][(x as i32 + dx1) as usize]
+                    && letter == grid[(y as i32 + dy2) as usize][(x as i32 + dx2) as usize]
+                    && letter
+                        != grid[(y as i32 + dy1 + dy2) as usize][(x as i32 + dx1 + dx2) as usize]
+                {
+                    // corner like this: _|X
+                    // (exterior)        X X
+                    corners += 1;
+                } else if check_inbounds(
+                    (x as i32 + dx1),
+                    (y as i32 + dy1),
+                    width as i32,
+                    height as i32,
+                ) && check_inbounds(
+                    (x as i32 + dx2),
+                    (y as i32 + dy2),
+                    width as i32,
+                    height as i32,
+                ) && letter != grid[(y as i32 + dy1) as usize][(x as i32 + dx1) as usize]
+                    && letter != grid[(y as i32 + dy2) as usize][(x as i32 + dx2) as usize]
+                {
+                    // corner like this   _
+                    // (interior)        |X
+                    corners += 1;
+                }
+            }
+        }
+    }
+    corners
 }
--- day-12/p2/tests/tests.rs
+++ day-12/p2/tests/tests.rs
@@ -2,127 +2,89 @@ use p2::*;
 use std::collections::HashSet;
 
 #[test]
-fn test_example_1() {
-    let input = "AAAA\nBBCD\nBBCC\nEEEC\n\r";
-    let solution = 80;
-    assert_eq!(calculate_price(input), solution);
-}
-
-#[test]
-fn test_example_2() {
-    let input = "EEEEEn\nEXXXX\nEEEEE\nEXXXX\nEEEEE";
-    let solution = 236;
-    assert_eq!(calculate_price(input), solution);
-}
-
-#[test]
-fn test_example_3() {
-    let input = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE";
-    let solution = 1206;
-    assert_eq!(calculate_price(input), solution);
-}
-
-#[test]
-fn test_example_4() {
-    let input = "AAAAAA\nAAABBA\nAAABBA\nABBAAA\nABBAAA\nAAAAAA\n";
-    let solution = 368;
-    assert_eq!(calculate_price(input), solution);
-}
-
-#[test]
-fn test_single_cell_region() {
-    // A single 'A' cell. Area=1.
-    // It's just one square, so its boundary is a 1x1 bounding box = 4 edges.
-    // But as a single rectangle, those 4 edges form exactly 4 sides (no collinearity merges).
-    // Under Part Two logic: cost = area (1) * sides (4) = 4.
-    let input = "A";
+fn test_corner_count1() {
+    let input = "\
+AAAA
+";
     let grid = str_to_vec(input);
-    let mut visited = HashSet::new();
-    let cost = calculate_single_price(0, 0, &grid, &mut visited);
-    assert_eq!(cost, 4, "Expected 4 for a 1x1 region");
+    let solution = 4;
+    let calculated = count_corners(&grid);
+    assert_eq!(calculated, solution);
 }
 
 #[test]
-fn test_2x2_all_a() {
-    // 2x2 block of 'A':
-    // AA
-    // AA
-    //
-    // The area is 4.
-    // It's one big 2-wide-by-2-tall rectangle, which has 4 sides total:
-    //   - top horizontal
-    //   - bottom horizontal
-    //   - left vertical
-    //   - right vertical
-    // So cost = 4 (area) * 4 (sides) = 16.
+fn test_corner_count2() {
+    let input = "\
+AAAA
+AAAA
+";
+    let grid = str_to_vec(input);
+    let solution = 4;
+    let calculated = count_corners(&grid);
+    assert_eq!(calculated, solution);
+}
+
+#[test]
+fn test_corner_count3() {
+    let input = "\
+AAAB
+";
+    let grid = str_to_vec(input);
+    let solution = 8;
+    let calculated = count_corners(&grid);
+    assert_eq!(calculated, solution);
+}
+
+#[test]
+fn test_corner_count4() {
+    let input = "\
+AAAB
+AAAB
+";
+    let grid = str_to_vec(input);
+    let solution = 8;
+    let calculated = count_corners(&grid);
+    assert_eq!(calculated, solution);
+}
+
+#[test]
+fn test_corner_count5() {
     let input = "\
 AA
 AA
+AA
+BB
 ";
     let grid = str_to_vec(input);
-    let mut visited = HashSet::new();
-    let cost = calculate_single_price(0, 0, &grid, &mut visited);
-    assert_eq!(cost, 16, "Expected 16 for a 2x2 region");
+    let solution = 8;
+    let calculated = count_corners(&grid);
+    assert_eq!(calculated, solution);
 }
 
 #[test]
-fn test_3x1_row_of_a() {
-    // A single row of 'A': "AAA"
-    //
-    // The area is 3.
-    // As a 3-wide-by-1-tall rectangle, it has 4 sides total:
-    //   - top horizontal
-    //   - bottom horizontal
-    //   - left vertical
-    //   - right vertical
-    // So cost = 3 (area) * 4 (sides) = 12.
-    let input = "AAA";
-    let grid = str_to_vec(input);
-    let mut visited = HashSet::new();
-    let cost = calculate_single_price(0, 0, &grid, &mut visited);
-    assert_eq!(cost, 12, "Expected 12 for a 3x1 region");
-}
-
-#[test]
-fn test_small_with_two_regions() {
-    // A 2x2 grid but with half 'A' and half 'B', like:
-    // AB
-    // AB
-    //
-    // Region 1 (all 'A'): (0,0) and (0,1).
-    //   - That's a 2-tall-by-1-wide rectangle => area=2, sides=4 => cost=8.
-    // Region 2 (all 'B'): (1,0) and (1,1).
-    //   - Same shape => area=2, sides=4 => cost=8.
-    //
-    // If we call calculate_single_price(0,0), we only get Region 1’s cost = 8.
-    // But that’s correct for that one region. So we expect 8 from that call.
+fn test_corner_count6() {
     let input = "\
-AB
-AB
+AAAA
+AAAA
+AAAA
+BBAA
 ";
     let grid = str_to_vec(input);
-    let mut visited = HashSet::new();
-    // We'll compute the region that starts at (0,0) => the 'A' region
-    let cost_a_region = calculate_single_price(0, 0, &grid, &mut visited);
-    assert_eq!(
-        cost_a_region, 8,
-        "Expected 8 for the 'A' region in a 2x2 split"
-    );
-
-    // Next, if we want to test the 'B' region in isolation, do the same from (1,0).
-    let cost_b_region = calculate_single_price(1, 0, &grid, &mut visited);
-    assert_eq!(
-        cost_b_region, 8,
-        "Expected 8 for the 'B' region in a 2x2 split"
-    );
+    let solution = 10;
+    let calculated = count_corners(&grid);
+    assert_eq!(calculated, solution);
 }
 
 #[test]
-fn test_single_area() {
-    let input = "RRRRIICCFF\nRRRRIICCCF\nVVRRRCCFFF\nVVRCCCJFFF\nVVVVCJJCFE\nVVIVCCJJEE\nVVIIICJJEE\nMIIIIIJJEE\nMIIISIJEEE\nMMMISSJEEE";
+fn test_corner_count7() {
+    let input = "\
+AAAA
+ABAA
+ABAA
+AAAA
+";
     let grid = str_to_vec(input);
-    let mut visited = HashSet::new();
-    let (x, y) = (1, 1);
     let solution = 12;
-    assert_eq!(find_single_area(x, y, &grid, &mut visited), solution);
+    let calculated = count_corners(&grid);
+    assert_eq!(calculated, solution);
 }
